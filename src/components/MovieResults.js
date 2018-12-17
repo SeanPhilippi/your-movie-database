ed54220513b49342b96a03f3abc8ad2ada373daa
SeanPhilippi <sean.philippi@gmail.com>
2018-12-16 18:23:46 -0600

    working on rendering search results

---
+++ src/components/MovieResults.js
@@ -0,0 +1,12 @@
+// might not want this in it's own component for ease
+
+import React from 'react';
+
+class MovieResults extends React.Component {
+
+  state = {
+
+  }
+}
+
+export default MovieResults;
--- src/components/Search.js
+++ src/components/Search.js
@@ -6,15 +6,26 @@ class Search extends React.Component {
     searchText: '',
     apiUrl: 'http://www.omdbapi.com/?',
     apiKey: 'd5d74a24&',
+    rows: [],
     results: []
+  }
+
+  inputStyle = {
+    fontSize: 22,
+    display: 'block',
+    paddingLeft: 16,
+    margin: 'auto',
+    marginTop: 20,
+    width: '42%'
   }
 
   handleSearch = () => {
     const { searchText, apiUrl, apiKey } = this.state;
-    fetch(`${apiUrl}s=${searchText}apikey=${apiKey}`)
+    fetch(`${apiUrl}s=${searchText}&apikey=${apiKey}`)
       .then(res => res.json())
       .then(res => this.setState({ results: res.Search }))
       .catch(err => console.error(err));
+    this.renderResults();
   }
 
   onTextChange = e => {
@@ -27,14 +38,39 @@ class Search extends React.Component {
     }
   }
 
+  renderResults = () => {
+    const movieRows = this.state.results.map(movie => {
+      return (
+        <p key={movie.imdbID}>
+          {movie.Title} ({movie.Year})
+        </p>
+      )
+    })
+    this.state = { results: movieRows };
+    console.log('results2', this.state.results);
+  }
+
   render() {
+    // if (this.state.results) {
+    //   resultsContent = this.state.results;
+    //   console.log('results content', resultsContent)
+    // } else {
+    //   resultsContent = null;
+    // }
+
     return (
       <div className="Search">
         <input
           type="text"
-          value="ADD A FILM..."
-        // search={this.handleSearch}
+          placeholder="Add a film..."
+          style={this.inputStyle}
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          onKeyUp={this.onKeyUp}
         />
+        <div>
+          {this.renderResults}
+        </div>
       </div>
     )
   }
