c57c17765b2f09143b8fad8dd53609258541b6fa
SeanPhilippi <sean.philippi@gmail.com>
2019-01-21 14:56:37 -0600

    styling with css now, grid implemented

--- src/components/SortableComponent.js
+++ src/components/SortableComponent.js
@@ -6,32 +6,6 @@ import { orderList, deleteMovie } from '../redux/actions';
 
 class SortableComponent extends Component {
 
-  liStyle = {
-    fontSize: '15px',
-    border: 'black solid 1px',
-    margin: 'auto',
-    paddingTop: '2px',
-    paddingBottom: '2px',
-    marginTop: '10px',
-    marginBottom: '10px',
-    display: 'block',
-    width: '42%'
-  };
-
-  buttonStyle = {
-    fontSize: '20px',
-    // border: 'black solid 2px',
-    float: 'right',
-    marginRight: '5px',
-    padding: '0 7px 3px 7px'
-  }
-
-  numStyle = {
-    fontSize: '22px',
-    float: 'left',
-    marginLeft: '10px'
-  }
-
   // save a list
   handleSave = () => {
     fetch('/:user/list', {
@@ -62,15 +36,26 @@ class SortableComponent extends Component {
     const SortableItem = SortableElement(({ movie, sortIndex }) => {
       return (
 
-        <div key={movie.id} style={this.liStyle} >
-          <span>
-            <span style={this.numStyle}>{sortIndex + 1} |</span>
-            <span style={{ fontSize: "20px" }}>{movie.name}</span>
-          </span>
+        <div
+          key={movie.id}
+          className="movie-item"
+        >
+          <div className="numbers">{sortIndex + 1} |</div>
+          <div className="movie-info">
+            <div style={{ fontSize: "20px" }}>
+              {movie.name}
+            </div>
+            <div className="dir-year">
+              {movie.director}, {movie.year}
+            </div>
+          </div>
           {/* delete button */}
-          <button onClick={() => deleteMovie(movie)} style={this.buttonStyle}>x</button>
-          <br />
-          {movie.director}, {movie.year}
+          <button
+            onClick={() => deleteMovie(movie)}
+            className="delete"
+          >
+            ✕
+          </button>
         </div>
       )
     }
@@ -83,7 +68,14 @@ class SortableComponent extends Component {
           {
             items.map((movie, index) => {
               console.log('movie1', movie);
-              return <SortableItem key={`item-${movie.id}`} sortIndex={index} index={index} movie={movie} />
+              return (
+                <SortableItem
+                  key={`item-${movie.id}`}
+                  sortIndex={index}
+                  index={index}
+                  movie={movie}
+                />
+              )
             })
           }
         </ul>
