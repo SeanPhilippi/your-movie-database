63391099d2748ad27ca74155710fc1d3e3478dc5
Sean Philippi <sean.philippi@gmail.com>
2019-02-12 21:29:49 -0600

    port error fix

--- server/server.js
+++ server/server.js
@@ -26,6 +26,6 @@ app.get('/express', (req, res) => {
 });
 
 app.listen(PORT, err => {
-  if (err) console.log(err);
-  else console.log(`Server started on port ${port}`);
+  if (err) console.log(err)
+  else console.log(`Server started on port ${PORT}`);
 })
