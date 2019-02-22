380ee133f88f03c1e63c162a79b288557228eb21
Sean Philippi <sean.philippi@gmail.com>
2019-02-21 22:42:03 -0600

    adjust production path in server.js

--- server/server.js
+++ server/server.js
@@ -13,13 +13,7 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.use(express.static('../public/build'));
 }
 
 // Connect to Mongo
