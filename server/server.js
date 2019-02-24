09cd66d98648047656789918854f41b0feaf0ac7
Matt Viteri <matt.viteri@volusion.com>
2019-02-23 23:35:56 -0600

    fixing index files and package json

--- server/server.js
+++ server/server.js
@@ -11,16 +11,14 @@ const PORT = process.env.PORT || 4300;
 app.use(bodyParser.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+// Express will serve up production assets
+app.use(express.static('build'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Express serve up index.html file if it doesn't recognize route
+const path = require('path');
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+});
 
 // Connect to Mongo
 mongoose.connect(
