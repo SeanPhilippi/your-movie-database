8a586dee6d682c64fd9ee40495f6c0a82112ec89
Sean Philippi <sean.philippi@gmail.com>
2019-02-24 14:01:17 -0600

    removed double path declaration

--- server/server.js
+++ server/server.js
@@ -17,7 +17,6 @@ app.use(cors());
 app.use(express.static('build'));
 
 // Express serve up index.html file if it doesn't recognize route
-const path = require('path');
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 });
