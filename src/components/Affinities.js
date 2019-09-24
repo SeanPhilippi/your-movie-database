78340f5da2a1115a118fa9c4f5ef7476606dc146
Sean Philippi <sean.philippi@gmail.com>
2019-09-24 11:21:56 -0500

    data no longer transferring between profiles, table height increased

--- src/components/Affinities.js
+++ src/components/Affinities.js
@@ -1,30 +1,28 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-class Affinities extends PureComponent {
-  render() {
-    return (
-      <div>
-        <div className="bg-white">
-          {
-            this.props.affinities.slice(0, 5).map(
-              match => (
-                <div className="d-flex justify-content-between">
-                  <div className="bd-light col-10">
-                    <Link to={`/profile/${ match.username }`}>{ match.username }</Link>
-                  </div>
-                  <div className="bd-light col-2 text-right">
-                    { match.score }%
-                  </div>
-                </div>
-              )
-            )
-          }
-        </div>
-      </div>
-    )
-  }
-}
+const Affinities = ({ affinities }) => (
+  <div className="bg-white">
+    {
+      affinities.slice(0, 5).map(
+        match => (
+          <div className="d-flex justify-content-between">
+            <div className="bd-light row-height col-10">
+              <Link to={`/profile/${ match.username }`}>{ match.username }</Link>
+            </div>
+            <div className="bd-light row-height col-2 text-right">
+              { match.score }%
+            </div>
+          </div>
+        )
+      )
+    }
+  </div>
+)
+
+Affinities.propTypes = {
+  affinities: PropTypes.array
+};
 
 export default Affinities;
