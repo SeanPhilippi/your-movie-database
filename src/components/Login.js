69c393ce6f32b2a254da15f1b34e0f2c07e4f2e3
Sean Philippi <sean.philippi@gmail.com>
2019-12-16 22:16:10 -0600

    functional components

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,25 +5,21 @@ import { Col } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-class Login extends PureComponent {
-  render() {
-    return (
-      <div className="d-flex border-0 justify-content-center" style={{ flex: 1 }}>
-        <div className="inner-container">
-          <Col className="bg-white pt-2">
-            <CardWrapper
-              icon="sign-in-alt"
-              title="Login"
-              color="tan"
-            >
-              <LoginBox />
-            </CardWrapper>
-          </Col>
-        </div>
-      </div>
-    );
-  };
-};
+const Login = () => (
+  <div className="d-flex border-0 justify-content-center" style={{ flex: 1 }}>
+    <div className="inner-container">
+      <Col className="bg-white pt-2">
+        <CardWrapper
+          icon="sign-in-alt"
+          title="Login"
+          color="tan"
+        >
+          <LoginBox />
+        </CardWrapper>
+      </Col>
+    </div>
+  </div>
+);
 
 Login.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired
--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,21 +1,18 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-class PageNotFound extends PureComponent {
-  render() {
-    return (
-      <div className="page-not-found w-100 d-flex align-items-center justify-content-center" style={{ flex: 1 }}>
-        <div className="img-404">
-        </div>
-        <div className="page-not-found-text pb-4">
-          <div className="font-weight-bold">Page Not Found</div>
-          <p style={{ fontSize: '1rem' }}>
-            There is nothing here.  Click <NavLink className="link" to="/"><b>here</b></NavLink> to return to the home page.
-          </p>
-        </div>
-      </div>
-    )
-  }
-}
+const PageNotFound = () => (
+  <div className="page-not-found w-100 d-flex align-items-center justify-content-center" style={{ flex: 1 }}>
+    <div className="img-404">
+    </div>
+    <div className="page-not-found-text pb-4">
+      <div className="font-weight-bold">Page Not Found</div>
+      <p style={{ fontSize: '1rem' }}>
+        There is nothing here.  Click <NavLink className="link" to="/"><b>here</b></NavLink> to return to the home page.
+      </p>
+    </div>
+  </div>
+);
+
 
 export default PageNotFound;
