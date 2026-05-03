7372edf8aa1bbae19412942d690a7cdba76822d6
SeanPhilippi <sean.philippi@gmail.com>
2026-05-03 16:07:20 -0400

    fix for crash when refreshing on a user profile and fetchCurrentUser was being called

--- src/utils/api/api.users.js
+++ src/utils/api/api.users.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export const get = {
-  newRegisters: () => axios('api/users/new-registers'),
-  currentUser: () => axios('api/users/current'),
+  newRegisters: () => axios('/api/users/new-registers'),
+  currentUser: () => axios('/api/users/current'),
   settings: () => axios.get('/api/users/settings'),
   searchUsers: query => axios.get(`/api/users/search?q=${encodeURIComponent(query)}`),
 };
 
 export const post = {
   newUser: user => axios.post('/api/users/register', user),
-  login: user => axios.post('api/users/login', user),
+  login: user => axios.post('/api/users/login', user),
   forgotPassword: data => axios.post('/api/users/forgot-password', data),
   resetPassword: (token, data) => axios.post(`/api/users/reset-password/${token}`, data),
   unsubscribe: (token, category) =>
